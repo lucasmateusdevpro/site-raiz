a311287e118dc00c01953105c057f966c233011c
Loja Santa Fé <lojasantaf@intranet.nr>
2025-05-19 17:43:36 -0300

    responcivo1

---
+++ js/script3.js
@@ -0,0 +1,46 @@
+
+
+
+function selectCor1(valor) {
+    const img1 = document.getElementById("logo1")
+    const img2 = document.getElementById("logo2")
+    const body = document.getElementsByTagName("body")
+    const favicon = document.getElementById("favicon")
+
+    if (valor === "white") {
+        favicon.href = "assets/favicon/favicon.ico"
+        img2.src = "assets/images/baner.png"
+        img1.src = "assets/images/logo1.png"
+        document.body.style.background = "#FFFFFF";
+        localStorage.setItem("corSelecionada", "white"); 
+
+    }
+
+    if (valor === "black") {
+        favicon.href = "assets/favicon/favicon-black.ico"
+        img2.src = "assets/images/baner-black.png"
+        img1.src = "assets/images/logo1-black.png"
+        document.body.style.background = "#1F262E";
+        localStorage.setItem("corSelecionada", "black"); 
+
+    }
+
+}
+
+window.addEventListener("DOMContentLoaded", function () {
+    const corSalva = localStorage.getItem("corSelecionada");
+
+    if (corSalva === "white") {
+        document.body.style.background = "#FFFFFF";
+        document.getElementById("logo1").src = "assets/images/logo1.png";
+        document.getElementById("logo2").src = "assets/images/baner.png"
+        document.getElementById("favicon").href = "assets/favicon/favicon.ico"
+    }
+
+    if (corSalva === "black") {
+        document.body.style.background = "#1F262E";
+        document.getElementById("logo1").src = "assets/images/logo1-black.png"
+        document.getElementById("logo2").src = "assets/images/baner-black.png"
+        document.getElementById("favicon").href = "assets/favicon/favicon-black.ico"
+    }
+});
