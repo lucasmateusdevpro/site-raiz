5705ec8ea4269938c7c4cdb583d483b6e1abf90a
lucas mateus <lucas.mateus6301148@gmail.com>
2025-05-22 16:16:23 -0300

    teste troca de pastas

--- teste.js
+++ teste.js
@@ -1,6 +1,6 @@
  
 
-/* let num = 0
+ let num = 0
 
 function iniciar(){
 
@@ -16,9 +16,13 @@ function iniciar(){
 }
 
 
- */
 
 
+vdçlhsçaljhqçfgvalçsfmnvoqfifgujpçqlenkg poasjif   sfoyhqt weroifcjwqe fqowierf fpoiwejrfm ewfpowerf isso pe apenas um tesxte pra gente ver se funfa oque eu estou pesnaod em fazer 
+kkkk a parte engracasda pe que ate parade que que etestou realemnte digitandfo tudo serto oldflkrwofh   w jf fcuioed dfujeklujwrngflkfg78ermne78ef rfioerfikefmjdfijherfnrf  
+wjgweiugrw rwuidhf foiyhhfrffgç~sfoliknergncvj[pojr wrepoihrtipohrt wrpoiuert fgrpjrtf rtfpof fopher fhdf fhf mssghfmfpçifr fifie dfisls aied fof f8fd fis cvjuedle vbnwpçedur  vamos ver como esta saindo minhas escritas por aqui 
+     bem mais eu sei que isso pode ter muitas letras erradas porquie este teclado esrta com as teclas rudoas errada mais s parte enfrasad é que a camera esta vendo isso e tentando intender oque esta acontecxend0o kkkf oih mais eu estou amondo tudo isso que esdta acontecendo, olha mais pra ser bem realizta até que eu estou escrenendo bem, olha ali eu até cololoquei uns acebtos kkkkkk eu so sei que isso vi dar un trabalho pra  porra ora apafamr
+]
 
 
 
